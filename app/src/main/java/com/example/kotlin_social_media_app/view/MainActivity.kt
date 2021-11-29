0988d971b876c0d1e67ec4443aeb755a9c002c29
achmadrizkin <arizki.nf02@gmail.com>
2021-11-29 19:41:38 +0700

    Add dependencies, and etc ...

--- app/src/main/java/com/example/kotlin_social_media_app/view/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_social_media_app/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kotlin_social_media_app
+package com.example.kotlin_social_media_app.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlin_social_media_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

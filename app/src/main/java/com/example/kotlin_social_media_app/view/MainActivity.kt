519e635db5ae4a000206d912d0e70de1102c7258
achmadrizkin <arizki.nf02@gmail.com>
2021-11-30 11:17:04 +0700

    Make Bottom Nav and etc

--- app/src/main/java/com/example/kotlin_social_media_app/view/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_social_media_app/view/MainActivity.kt
@@ -5,8 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.kotlin_social_media_app.R
-import com.example.kotlin_social_media_app.view.search.SearchActivity
-import dagger.hilt.android.AndroidEntryPoint
+import com.example.kotlin_social_media_app.view.bottomNav.BottomNavActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Handler().postDelayed({
-            val signInIntent = Intent(this, SearchActivity::class.java)
+            val signInIntent = Intent(this, BottomNavActivity::class.java)
             startActivity(signInIntent)
             finish()
         }, 1000)
---
+++ app/src/main/java/com/example/kotlin_social_media_app/view/bottomNav/BottomNavActivity.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin_social_media_app.view.bottomNav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.kotlin_social_media_app.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class BottomNavActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_nav)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
